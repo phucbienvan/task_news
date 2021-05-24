1e04067ce1e947171e6dfc03f5986448997d2001
Phúc Biện <bienvanphuc98@gmail.com>
2021-05-24 23:50:36 +0700

    update database

---
+++ database/migrations/2021_05_24_162257_create_news_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->string('name');
+            $table->integer('category_id');
+            $table->text('desc');
+            $table->text('content');
+            $table->string('image');
+            $table->integer('status');
+            $table->integer('views');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
